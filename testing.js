256aafafcdf6fc2fdbe540ad9190689c8e310d4e
jgstew <james@jgstew.com>
2015-03-07 18:00:04 -0500

    Update testing.js

--- testing.js
+++ testing.js
@@ -1,4 +1,5 @@
 
+// https://github.com/jgstew/remote-relevance
 // http://stackoverflow.com/questions/5869216/how-to-store-node-js-deployment-settings-configuration-files
 var config = require('./config.json');
 
