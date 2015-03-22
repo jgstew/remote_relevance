95c8c1877bc4d9f042fa3cad527f1d98511bad9d
JGStew <james@jgstew.com>
2015-03-22 23:55:21 +0000

    working on sending actions

--- python/test_receive_results.py
+++ python/test_receive_results.py
@@ -1,6 +1,7 @@
 
 from flask import Flask
 import json
+from BES_CONFIG import *
 
 app = Flask(__name__)
 
@@ -12,7 +13,7 @@ app = Flask(__name__)
 # https://en.wikipedia.org/wiki/Base64
 # http://stackoverflow.com/questions/3470546/python-base64-data-decode
 #  concatenations "~" of (base64 encode it) of unique values of (it as string) of (names of regapps)
-@app.route('/remote/query/<path:bes_result>')
+@app.route('/remote/results/<path:bes_result>')
 def rest_bes_query_result(bes_result):
     result = ""
     for string in bes_result.split("~"):
@@ -20,6 +21,11 @@ def rest_bes_query_result(bes_result):
     print result
     #return "thanks!"
     return "<b>result:</b> " + bes_result + " <br/><b>base64decode:</b> " + result
+
+@app.route('/remote/query/<path:bes_query>')
+def rest_bes_query(bes_query):
+    print "https://" + BES_ROOT_SERVER_DNS + ":" + BES_ROOT_SERVER_PORT + "/api/"
+    return bes_query + " "
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
