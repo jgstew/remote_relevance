83541a76d00232cf34ecb3c02934826178f64eb0
jgstew <james@jgstew.com>
2015-03-24 14:42:11 -0400

    Create BES_CONFIG.py

---
+++ python/BES_CONFIG.py
@@ -0,0 +1,9 @@
+
+#  This file is in .gitignore for this repo to prevent changes being pushed
+
+BES_ROOT_SERVER_DNS  = "root.domain.tld"
+BES_ROOT_SERVER_PORT = "52311"
+BES_USER_NAME = "user"
+BES_COMPUTER_GROUP = "00000"
+REMOTE_RELEVANCE_SERVER = "SET_TO_THE_URL_OF_THIS_REMOTE_RELEVANCE_SERVER:WITH_PORT_NUMBER"
+
